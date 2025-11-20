c5a7d8a98d6bb8d4622d923514f86ca13ca16a9a
Vishal Gupta <your-email@example.com>
2025-11-21 01:13:54 +0530

    changessssss

--- src/component/PromocardSection.jsx
+++ src/component/PromocardSection.jsx
@@ -61,7 +61,7 @@ const PromocardSection = () => {
         <div className="max-w-7xl mx-auto px-6">
           <div className="mb-10">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-800 tracking-tight">
-              Explore Popular Services
+              Explore Popular Product
             </h2>
           </div>
           <div>No services available</div>
